eed86639ecddd3ef829d4b335205628135b6cd3d
Pranali Dhumankhede <pranalidhumankhede@gmail.com>
2026-09-21 11:48:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,18 @@
 import streamlit as st
-import joblib
-import numpy as pd
+import numpy as np
+import pickle
 
-# load model
-model = joblib.load('iris_model.pkl')
+# Your inputs
+sepal_length = st.number_input("Sepal Length")
+sepal_width = st.number_input("Sepal Width")
+petal_length = st.number_input("Petal Length")
+petal_width = st.number_input("Petal Width")
 
-#page title
-st.title('iris flower Prediction App')
+input_data = np.array([[
+    sepal_length,
+    sepal_width,
+    petal_length,
+    petal_width
+]])
 
-st.header('Enter the measurements of the Iris Flower')
-
-#Input labels
-sepal_length = st.number_input('Sepal length (cm)', min_value=0.0, max_value=8.0, value=5.0, step=0.1)
-sepal_width = st.number_input('Sepal width (cm)', min_value=0.0, max_value=5.0, value=3.0, step=0.1)
-petal_length = st.number_input('Petal length (cm)', min_value=0.0, max_value=8.0, value=4.0, step=0.1)
-petal_width = st.number_input('Petal width (cm)', min_value=0.0, max_value=3.0, value=1.0, step=0.1)
-
-#Prediction
-if st.button('Predict'):
-    input_data = np.array([[sepal_length,
-                            sepal_width,
-                            petal_length,
-                            petal_width]]).astype(np.float64)
-    prediction = model.predict(input_data)
-    st.success(f'The predicted Iris species is: {prediction[0]}')         
+prediction = model.predict(input_data)
